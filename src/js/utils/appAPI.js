2acab1a19ada3ca082b6bb53564872f377f37925
martin byrne <marb61a@gmail.com>
2016-04-10 18:17:28 +0000

    updated appApi

--- src/js/utils/appAPI.js
+++ src/js/utils/appAPI.js
@@ -2,5 +2,28 @@ var Firebase = require('firebase');
 var AppActions = require('../actions/AppActions');
 
 module.exports = {
+	saveContact : function(contact){
+	    this.firebaseRef = new Firebase('https://reactcontactsmanager.firebaseio.com/contacts');
+		this.firebaseRef.push({
+			contact: contact
+		});
+	},
 	
+	getContacts : function(){
+	    this.firebaseRef = new Firebase('https://reactcontactsmanager.firebaseio.com/contacts');
+	    this.firebaseRef.once("value", function(snapshot){
+	        var contacts = [];
+	        snapshot.forEach(function(childSnapshot){
+	            var contact = {
+	                id: childSnapshot.key(),
+					name: childSnapshot.val().contact.name,
+					phone: childSnapshot.val().contact.phone,
+					email: childSnapshot.val().contact.email
+	            };
+	            
+	            contacts.push(contact);
+	            AppActions.receiveContacts(contacts);
+	        });
+	    });
+	}
 };
