e457e7342cf89d9ad0bb0db313f862dc5ac18306
martin byrne <marb61a@gmail.com>
2016-04-12 10:25:05 +0000

    updated AppSotre switch cases

--- src/js/stores/AppStore.js
+++ src/js/stores/AppStore.js
@@ -2,38 +2,33 @@ var AppDispatcher = require("../dispatcher/AppDispatcher");
 var AppConstants = require("../constants/AppConstants");
 var EventEmitter = require("events").EventEmitter;
 var assign = require("object-assign");
-var appAPI = require('../utils/appAPI.js');
+var AppAPI = require('../utils/appAPI.js');
 
 var CHANGE_EVENT = 'change';
 
 var _contacts = [];
+var _contact_to_edit = '';
 
 var AppStore = assign({}, EventEmitter.prototype, {
     getContacts: function(){
 		return _contacts;
 	},
-	
 	saveContact: function(contact){
 		_contacts.push(contact);
 	},
-	
 	setContacts: function(contacts){
 		_contacts = contacts;
 	},
-	
 	removeContact : function(contactId){
 	    var index = contacts.findIndex(x => x.id === contactId);
 	    _contacts.splice(index, 1);
 	},
-	
 	setContactToEdit : function(contact){
 	    _contact_to_edit = contact;
 	},
-	
 	getContactToEdit : function(){
 	    return _contact_to_edit;
 	},
-	
 	updateContact : function(contact){
 	    for(i = 0; i < contacts.length; i++){
 	        if(_contacts[i].id == contact.id){
@@ -42,15 +37,12 @@ var AppStore = assign({}, EventEmitter.prototype, {
 	        }
 	    }
 	},
-	
     emitChange : function(){
         this.emit(CHANGE_EVENT);  
     },
-    
     addChangeListener : function(callback){
         this.on('change', callback);
     },
-    
     removeChangeListener: function(callback){
 		this.removeListener('change', callback);
 	}
@@ -58,16 +50,18 @@ var AppStore = assign({}, EventEmitter.prototype, {
 
 AppDispatcher.register(function(payload){
     var action = payload.action;
+    
     switch (action.actionType) {
         case AppConstants.SAVE_CONTACT:
             console.log('Saving Contact...');
             // Store save
             AppStore.saveContact(action.contact);
             // Save to API
-			appAPI.saveContact(action.contact);
+			AppAPI.saveContact(action.contact);
             // Emit Change
             AppStore.emit(CHANGE_EVENT);
             break;
+            
         case AppConstants.RECEIVE_CONTACTS:
             console.log('Receiving Contacts...');
 			// Store Save
@@ -75,7 +69,33 @@ AppDispatcher.register(function(payload){
 			//Emit Change
 			AppStore.emit(CHANGE_EVENT);
 			break;
+			
+		case AppConstants.REMOVE_CONTACT:
+		    console.log('Removing Contact...');
+		    // Store Remove
+		    AppStore.removeContact(action.contactId);
+		    // API Remove
+			AppAPI.removeContact(action.contactId);
+		    //Emit Change
+			AppStore.emit(CHANGE_EVENT);
+			break;
         
+        case AppConstants.EDIT_CONTACT:
+            // Store Remove
+			AppStore.setContactToEdit(action.contact);
+            //Emit Change
+			AppStore.emit(CHANGE_EVENT);
+			break;
+        
+        case AppConstants.UPDATE_CONTACT:
+            console.log('Updating Contact...');
+            // Store Update
+			AppStore.updateContact(action.contact);
+			// API Update
+			AppAPI.updateContact(action.contact);
+            //Emit Change
+			AppStore.emit(CHANGE_EVENT);
+			break;
     }
     
     return true;
